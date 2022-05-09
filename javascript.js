7d7dc99be8aa4ecdd6f14c145402237d16f92883
Omar <omarhlode@gmail.com>
2022-05-09 09:33:52 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,52 @@
+/* localStorage.setItem("fullname", "Charles Widmore");
+ const fullname = localStorage.getItem("fullname");
+console.log(fullname);
+localStorage.removeItem("fullname");
+const fullnameBorrado = localStorage.getItem("fullname");
+console.log(fullnameBorrado);  */
+
+onRequest();
+
+async function onRequest() {
+  const jwt = localStorage.getItem("jwt");
+  try {
+    const response = await fetch("http://localhost:1337/api/users/me", {
+      method: "GET",
+      headers: { Authorization: "Bearer " + jwt },
+    });
+
+    if (!response.ok) {
+      const message = `Error: ${response.status}`;
+      throw new Error(message);
+    }
+    const data = await response.json();
+    printData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function printData(data) {
+  const container = document.getElementById("container");
+
+  const p = document.createElement("p");
+  const div = document.createElement("div");
+
+  p.textContent = "Bienvenido " + data.name + " " + data.subname;
+
+  div.appendChild(p);
+  container.appendChild(div);
+
+  //console.log(data);
+}
+const borrar = document.getElementById("btRequest2");
+
+borrar.addEventListener("click", () => {
+  sendData();
+});
+
+function sendData() {
+  localStorage.removeItem("jwt");
+
+  window.location.href = "login.html";
+}
---
+++ signup.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("container");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendData(form);
+});
+
+async function sendData(form) {
+  try {
+    const formData = new FormData(form);
+    const queryString = new URLSearchParams(formData).toString();
+    const response = await fetch(
+      "http://localhost:1337/api/auth/local/register",
+      {
+        method: "POST",
+        body: queryString,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    if (!response.ok) {
+      const message = `Error: ${response.status}`;
+      throw new Error(message);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    getData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function getData(data) {
+  //console.log(data.jwt);
+
+  localStorage.setItem("jwt", data.jwt);
+
+  window.location.href = "index.html";
+}
